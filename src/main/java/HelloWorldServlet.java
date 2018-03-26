1bf929792ce8f6b529e24c0a0f1822a880c9d372
alexander Bous <alexmbous@gmail.com>
2018-03-26 14:33:07 -0500

    first first first

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,10 +1,27 @@
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, World!</h1>");
 
+
+//
+//        String name = request.getParameter("name");
+//        out.println("<HTML>");
+//        out.println("<HEAD><TITLE>Hello, " + name + "</TITLE></HEAD>");
+//        out.println("<BODY>");
+//        out.println("Hello, " + name);
+//        out.println("</BODY></HTML>");
+//    }
+//    public String getServletInfo() {
+//        return "A servlet that knows the name of the person to whom it's" +
+//                "saying hello";
     }
+
+
+
+
 }
