f36d3f199425b077543f1d163045cd4982c0ea42
alexander Bous <alexmbous@gmail.com>
2018-04-03 10:31:23 -0500

    JDBC

---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads{
+    private Connection connection(Config config) {
+        return null;
+    }
+
+
+    @Override
+    public List<Ad> all() {
+        return null;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
+}
